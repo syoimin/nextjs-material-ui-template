8e6a46bd4b5d75b9b76176ac9017a8f04d5a4248
syoimin <syoimin@gmail.com>
2021-09-23 01:08:27 +0000

    feat: レイアウトコンポーネントの作成とレイアウトの適応

---
+++ components/layout.tsx
@@ -0,0 +1,12 @@
+import Sidebar from './sidebar'
+import Footer from './footer'
+
+export default function Layout({ children }) {
+    return (
+        <>
+            <Sidebar />
+            <main>{children}</main>
+            <Footer />
+        </>
+    )
+}
